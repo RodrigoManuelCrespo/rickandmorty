2063a0234665def635a95621f71cf359aec581d5
Rodrigo Crespo <cresporodrigomanuel@gmail.com>
2024-04-28 21:20:30 -0300

    Add jest test

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '@/redux/store';
+import Home from '@/app/page';
+import '@testing-library/jest-dom'
+
+describe('Home component', () => {
+    it('renders without crashing and contains welcome text', () => {
+        render(
+            <Provider store={store}>
+                <Home />
+            </Provider>
+        );
+
+        // Verificar si los textos existen
+        const welcomeText = screen.getByText(/Welcome to the ultimate Rick and Morty app!/i);
+        const characterText1 = screen.getByText(/Character #1/i);
+        const characterText2 = screen.getByText(/Character #2/i);
+        const episodesText = screen.getByText(/Episodes/i);
+
+        expect(welcomeText).toBeInTheDocument();
+        expect(characterText1).toBeInTheDocument();
+        expect(characterText2).toBeInTheDocument();
+        expect(episodesText).toBeInTheDocument();
+    });
+});
